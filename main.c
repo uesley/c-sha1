29e127077e8c40ab6ca60cf9720a5801c2db7f86
uesley <uesley.eumesmo@gmail.com>
2018-11-13 15:57:58 -0300

    implementa funcao de calcular os H's

--- main.c
+++ main.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define H1 0x67452301;
-#define H2 0xefcdab89;
-#define H3 0x98badcfe;
-#define H4 0x10325476;
-#define H5 0xc3d2e1f0;
-
 #define _message "CRYPTO";
 
 typedef unsigned int Word;
@@ -21,11 +15,10 @@ Word * createBlock(){
 	return block;
 }
 
-
 Block addLengthAtEndOfBlock(Block block, unsigned long int tam){
 	tam = tam * 8;
-	int p1 = (int) (tam >> 32);
-	int p2 = (int) tam;
+	int p1 = (unsigned int) (tam >> 32);
+	int p2 = (unsigned int) tam;
 	block[14] = p1;
 	block[15] = p2;
 	return block;
@@ -34,10 +27,10 @@ Block addLengthAtEndOfBlock(Block block, unsigned long int tam){
 Block text2NumericArray(char * text, int tam){
 	int charactere;
 	int block_pos;
-	int diff;
+	unsigned int diff;
 	int over = 1;
 	Word word;
-		Block block = (Block) createBlock();
+	Block block = (Block) createBlock();
 
 	for (charactere = 0; charactere < tam; charactere ++){
 		word = 0;
@@ -64,17 +57,91 @@ Block text2NumericArray(char * text, int tam){
 
 Block complete80(Block block){
 	int pos = 0;
-	int i;
-	int t1, t2;
 	do{ 
 		block[16 + pos] = block[13 + pos] ^ block[8 + pos];
 		block[16 + pos] = block[16 + pos] ^ block[2 + pos];
-		block[16 + pos] = block[16 + pos] ^ block[pos];	
-		// t1 = t1 << 1;
+		block[16 + pos] = block[16 + pos] ^ block[pos];
 		block[16 + pos] = (block[16 + pos] >> 31) | ( block[16 + pos] << 1); 
 		pos ++;
 	}while((pos + 16) < 80);
 	return block;
+}
+
+int Choose(unsigned int x, unsigned int y, unsigned int z){
+	return (x & y) ^ (~x & z);
+}
+
+int Parity(unsigned int x, unsigned int y, unsigned int z){
+	return (x ^ y) ^ z;
+}
+
+int Major(unsigned int x, unsigned int y, unsigned int z){
+	return (x & y) ^ (x & z) ^ (y & z);
+}
+
+int f_t(int t, unsigned int x, unsigned int y, unsigned int z){
+	if (t < 20){
+		return Choose(x, y, z);
+	}
+	if (t < 40){
+		return Parity(x, y, z);
+	}
+	if (t < 60){
+		Major(x, y, z);
+	}
+	return Parity(x, y, z);
+}
+
+int K_t(int t){
+	if (t < 20)
+		return 0x5a827999;
+	if (t < 40)
+		return 0x6ed9eba1;
+	if (t < 60)
+		return 0x8f1bbcdc;
+	return 0xca62c1d6;			
+} 
+
+unsigned int * intitializeH(){
+	unsigned int * H = malloc(sizeof(unsigned int)*5);
+	H[0] = 0x67452301;
+	H[1] = 0xefcdab89;
+	H[2] = 0x98badcfe;
+	H[3] = 0x10325476;
+	H[4] = 0xc3d2e1f0;
+	return H;
+}
+
+unsigned int * computeHsValues(Block block, unsigned int * H){
+	unsigned int T;
+	unsigned int a = H[0];
+	unsigned int b = H[1];
+	unsigned int c = H[2];
+	unsigned int d = H[3];
+	unsigned int e = H[4];
+	int t;
+
+	for (t = 0; t < 80; t++){		
+		T  = (a >> 27) | (a << 5); // rotate left 5 bits
+		T  = T + f_t(t, b, c, d);
+		T  = T + e;
+		T  = T + K_t(t);
+		T  = T + block[t];
+		e  = d;
+		d  = c;
+		c  = (b >> 2) | (b << 30); // rotate left 30 bits  /// testar isso
+		b  = a;
+		a  = T;
+		printf("[%i] :  a: %u , b: %u , c: %u , d: %u , e: %u \n", t, a, b, c, d, e);
+
+	}
+	H[0] += a;
+	H[1] += b;
+	H[2] += c;
+	H[3] += d;
+	H[4] += e;
+
+	return H;
 }
 
 
@@ -88,14 +155,28 @@ void printBlock(Block block){
 	}
 }
 
+void printDigest(unsigned int * H){
+	int i;
+	for(i = 0; i < 5; i++)
+		printf("%x",H[i]);
+	printf("\n");	
+}
 
 
 int main(int argc, char** argv) {
-	Block block = text2NumericArray("AB",2);
+	Block block = text2NumericArray("CRYPTO",6);
 	// block = addLengthAtEndOfBlock(block,2); 
 	block = complete80(block);
 	
-	printBlock(block);
+	// printBlock(block);
+	// printf("\n\nDigest: ");	
+	
+	unsigned int * H = intitializeH(); 
+	H = computeHsValues(block, H);
+	
+	printf("\nDigest: ");
+	printDigest(H);
+
     return (EXIT_SUCCESS);
 }
 
